289d7c080331c2e7dc5ceb93372c44b6bec8d2b5
Samuel <vaninisamuel.w@gmail.com>
2023-09-23 17:06:38 -0300

    Class game created.
    All the function of the game are in this class.
    For a cleaner main class.

--- BLACKJACK/src/DeckOfCardsTest.java
+++ BLACKJACK/src/DeckOfCardsTest.java
@@ -5,93 +5,26 @@ public class DeckOfCardsTest
    public static void main(String[] args)
    {
 	   
-	  Scanner s = new Scanner (System.in);
-	  int x = 0;
-	  int blackJackCount = 0;
-	  boolean stands = true;
+	  Scanner s1 = new Scanner (System.in);
+	  int qJogadores = 0;
 	  
 	  System.out.print("Quantos players irão jogar BlackJack? \n \nDigite o número de jogadores:");
-	  String str = s.nextLine();      
+	  String str = s1.nextLine();      
 
 	  try{
-           x = Integer.parseInt(str);
+           qJogadores = Integer.parseInt(str);
       }
       catch (NumberFormatException ex){
           ex.printStackTrace();
       }
 	  
 	  
-	  	Player p[] = new Player [x];
-		
-	  	DeckOfCards Deck = new DeckOfCards ();
-		
-	  	Deck.shuffle(); // place Cards in random order
-		  
-	  for (int i =0; i<p.length; i++)
-	  {	  
-	      System.out.print("Digite o nome do jogador:");
-	      String nome = s.nextLine();
-	      p[i] = new Player(nome, Deck);
-	      if(p[i].hasBlackjack())
-	      {
-	    	  System.out.println("Parabéns, " + p[i].getName() + ", você fez um BlackJack!");
-	    	  p[i].getCards();
-	    	  System.out.println("Soma das cartas: " + p[i].getTotal());
-	    	  blackJackCount ++;
-	      }
-	      
-	  }
-      
-     
-    
-     if(blackJackCount == 0)
-     {
-	      for (int i =0; i<p.length; i++)
-	      {
-	    	  stands = true;
-	    	  while (stands)
-	    	  {
-		    	  p[i].getCards();
-		    	  System.out.println("\nSoma das cartas: " + p[i].getTotal());
-		    	  
-		    	  if(p[i].isBust())
-	    		  {
-	    			  System.out.println("Você estorou!");
-	    			  sleep(4000);
-	    			  stands = false;
-	    		  }else {
-	    			  
-			    	  System.out.println("\n1 - Hit" + "		2 - Pass");
-	
-			    	  if(s.nextInt() == 2)
-			    	  { 
-			    		  stands = false;
-			    	  }
-			    	  else
-			    		  p[i].addCard(Deck);
-	    		  }
-	    	  }
-	    	  
-	      }     
-     } else if(blackJackCount == 1) {
-    	 System.out.println("Jogo encerrado, 1 vencedor");
-     } else {
-    	 System.out.println("Jogo empatado, " + blackJackCount + " vencedores!");
-     }
- 
-      
-      s.close();
+	  Game novo = new Game (qJogadores, s1);
+	  novo.startGame();
+      s1.close();
+	  
    } 
  // end class DeckOfCardsTest
 
-   /**
-    * Static method to spend time, before any other code.
-    */
-   public static void sleep (int time)
-	{
-		try {
-			Thread.sleep(time);
-		} catch (Exception e) {}
-		
-	}
+   
 }
